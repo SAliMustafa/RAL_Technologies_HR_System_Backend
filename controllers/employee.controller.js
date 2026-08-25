7cea16f947f50de50d94eea7b85a019d33af854a
mohasa7an22 <mohasar7an22@gmail.com>
2026-08-25 22:09:08 +0300

    made an update employee function

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -26,13 +26,13 @@ async function getAllEmployees(req,res) {
         if (department_id) filter.department_id = department_id
         const employees = await Employee.find(filter)
         .populate('company_id')
-      .populate('department_id')
-      .populate('designation_id')
-      .populate('reports_to', 'name_en name_ar employee_code')
-      .populate('holiday_list_id')
-      .populate('shift_type_id')
-      .populate('user', 'username role')
-      .sort({ createdAt: -1 })
+        .populate('department_id')
+        .populate('designation_id')
+        .populate('reports_to', 'name_en name_ar employee_code')
+        .populate('holiday_list_id')
+        .populate('shift_type_id')
+        .populate('user', 'username role')
+        .sort({ createdAt: -1 })
  
     res.status(200).json(employees)
     } catch(err) {
@@ -42,7 +42,49 @@ async function getAllEmployees(req,res) {
 
 async function getEmployeeById(req,res) {
     try{
+        const {employeeId} = req.params
+        if (!mongoose.Types.ObjectId.isValid(employeeId)) {
+            return res.stats(400).json({error: 'invalid employee id'})
+        }
+        const employee = await Employee.findById(employeeId)
+        .populate('company_id')
+        .populate('department_id')
+        .populate('designation_id')
+        .populate('reports_to', 'name_en name_ar employee_code')
+        .populate('holiday_list_id')
+        .populate('shift_type_id')
+        .populate('user', 'username role')
+        if(!employee) {
+            return res.status(404).json({ error: 'Employee not found'})
+        }
+        res.status(200).json(employee)
+    } catch(err){
+        res.status(500).json({error: err.message})
+    }
+}
 
+async function updateEmployee(req,res) {
+    try {
+        const {employeeId} = req.params
+        if(!mongoose.Types.ObjectId.isValid(employeeId)) {
+            return res.status(400).json({error: 'Employee not found'})
+        }
+        const employee = await Employee.findById(employeeId) 
+        if(!employee) {
+            return res.status(404).json({error: 'Employee not found in record'})
+        }
+        const {status} = req.body
+        if (status && !['active', 'on_leave', 'suspended', 'left'].includes(status)) {
+            return res.status(400).json({error: 'invalid status value'})
+        }
+        
+        Object.assign(employee, req.body)
+
+        if (status === 'left' && !req.body.date_of_leaving) {
+            employee.date_of_leaving = new Date()
+        }
+        await employee.save()
+        res.status(200).json(employee)
     } catch(err){
 
     }
