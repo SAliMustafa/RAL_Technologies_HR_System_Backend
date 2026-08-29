6bd358ed49b6a6b4e3684014cfac834df55085d1
mohasa7an22 <mohasar7an22@gmail.com>
2026-08-29 17:08:37 +0300

    merging with main

--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -88,22 +88,3 @@ router.get(
 
 
 module.exports = router
-const express = require('express')
-const router = express.Router()
-const verifyToken = require('../middleware/verifyToken')
-const { verifyHrAdmin } = require('../middleware/verifyRole')
-const {
-    createEmployee,
-    getAllEmployees,
-    getEmployeeById,
-    updateEmployee,
-    linkUserAccount
-} = require('../controllers/employee.controller')
-
-router.post('/', verifyToken, verifyHrAdmin, createEmployee)
-router.get('/', verifyToken, getAllEmployees)
-router.get('/:employeeId', verifyToken, getEmployeeById)
-router.put('/:employeeId', verifyToken, updateEmployee)
-router.patch('/:employeeId/link-user', verifyToken, linkUserAccount)
-
-module.exports = router
