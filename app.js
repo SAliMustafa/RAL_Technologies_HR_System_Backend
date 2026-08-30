78f49939eeca37066b6fd234f684258de71686a8
B1s0m <b0b.30916@gmail.com>
2026-08-30 08:48:20 +0300

    complete service check in

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const leaveAllocationRoutes = require('./routes/leaveallocation.routes')
 const userRoutes = require('./routes/user.routes')
 const EmployeeDocumentRoutes =require("./routes/employeeDocument.routes")
 const EmployeeRoutes =require("./routes/employee.routes")
+const checkInRoutes =require("./routes/checkIn.routes")
 // Middleware
 app.use(
     cors({
@@ -31,6 +32,7 @@ app.use('/leave', leaveTypeRoutes)
 app.use('/leave-allocation', leaveAllocationRoutes)
 app.use('/documents', EmployeeDocumentRoutes)
 app.use('/Employees', EmployeeRoutes)
+app.use('/checkIn', checkInRoutes)
 
 
 
