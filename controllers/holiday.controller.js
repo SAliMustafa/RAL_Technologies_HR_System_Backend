f743481fe83dbbfa20e2832f231ff712fc60a98e
a0m3 <almosawi0309@gmail.com>
2026-08-30 10:36:47 +0300

    getHolidays function in holiday controller created

--- controllers/holiday.controller.js
+++ controllers/holiday.controller.js
@@ -28,6 +28,27 @@ async function createHoliday(req, res) {
             })
         }
 
-        return res.status(500).json({message: 'Internal Server Error'})
+        return res.status(500).json({ message: 'Internal Server Error' })
+    }
+}
+
+async function getHolidays(req, res) {
+    try {
+        const { from_date, to_date } = req.query
+        const filter = {}
+
+        if (from_date || to_date) {
+            filter.date = {}
+            if (from_date) filter.date.$gte = new Date(from_date)
+            if (to_date) filter.date.$lte = new Date(to_date)
+        }
+
+        const holidays = await Holiday.find(filter).sort({ date: 1 })
+        return res.status(200).json(holidays)
+
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: "Internal Server Error" });
     }
 }
