eb9bd5ed94db11d57b74415c57dd60ae6c8a219c
a0m3 <almosawi0309@gmail.com>
2026-08-31 14:32:53 +0300

    fixes

--- app.js
+++ app.js
@@ -22,7 +22,23 @@ const holidayRoutes = require('./routes/holiday.routes')
 // Middleware
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || 'http://localhost:5173',
+        origin: (origin, callback) => {
+            const allowedOrigins = [
+                process.env.CLIENT_URL,
+                'http://localhost:5173',
+                'http://127.0.0.1:5173'
+            ]
+                .filter(Boolean)
+                .map((value) => value.replace(/\/$/, ''));
+
+            if (!origin || allowedOrigins.includes(origin.replace(/\/$/, ''))) {
+                callback(null, true);
+                return;
+            }
+
+            callback(new Error(`CORS blocked for origin: ${origin}`));
+        },
+        credentials: true,
     })
 );
 app.use(express.json())
