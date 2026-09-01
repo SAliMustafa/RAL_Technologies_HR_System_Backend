b4d32b3be4109abf45f0af7bb52044a89b543e0a
a0m3 <almosawi0309@gmail.com>
2026-09-01 21:52:07 +0300

    update in holiday model

--- models/Holiday.js
+++ models/Holiday.js
@@ -2,26 +2,43 @@ const mongoose = require("mongoose");
 
 const holidaySchema = new mongoose.Schema(
   {
-
-    date: {
+    from_date: {
       type: Date,
-      required: true
+      required: true,
+    },
+
+    to_date: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value >= this.from_date;
+        },
+        message: "to_date cannot be before from_date.",
+      },
     },
 
     description: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
 
     is_confirmed: {
       type: Boolean,
-      default: true
-    }
+      default: true,
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
 );
 
-holidaySchema.index({ date: 1 }, { unique: true })
+holidaySchema.virtual("days").get(function () {
+  const oneDayMs = 24 * 60 * 60 * 1000;
+  return Math.round((this.to_date - this.from_date) / oneDayMs) + 1;
+});
 
 module.exports = mongoose.model("Holiday", holidaySchema);
