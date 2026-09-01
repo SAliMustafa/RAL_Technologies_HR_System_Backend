54b076afdc127131266d2970738b70f1f598d42e
a0m3 <almosawi0309@gmail.com>
2026-09-01 21:52:46 +0300

    update in holiday controller

--- controllers/holiday.controller.js
+++ controllers/holiday.controller.js
@@ -3,21 +3,21 @@ const { logCreate, logUpdate } = require('../utils/auditLog')
 
 async function createHoliday(req, res) {
     try {
-        const { date, description, is_confirmed } = req.body
+        const { from_date, to_date, description, is_confirmed } = req.body
 
-        if (!date || !description) {
+        if (!from_date || !to_date || !description) {
             return res.status(400).json({
-                message: 'date and description are required.'
+                message: 'from_date, to_date and description are required.'
             })
         }
 
-        const holiday = await Holiday.create({ date, description, is_confirmed })
+        const holiday = await Holiday.create({ from_date, to_date, description, is_confirmed })
 
         logCreate({
             tableName: "holiday",
             recordId: holiday._id,
             userId: req.user._id,
-            data: { date, description, is_confirmed },
+            data: { from_date, to_date, description, is_confirmed },
             ipAddress: req.ip
         })
 
@@ -28,27 +28,13 @@ async function createHoliday(req, res) {
         if (err.name === "ValidationError") {
             return res.status(400).json({ message: err.message })
         }
-        if (err.code === 11000) {
-            return res.status(409).json({
-                message: 'A holiday already exists on this date.'
-            })
-        }
         return res.status(500).json({ message: 'Internal Server Error' })
     }
 }
 
 async function getHolidays(req, res) {
     try {
-        const { from_date, to_date } = req.query
-        const filter = {}
-
-        if (from_date || to_date) {
-            filter.date = {}
-            if (from_date) filter.date.$gte = new Date(from_date)
-            if (to_date) filter.date.$lte = new Date(to_date)
-        }
-
-        const holidays = await Holiday.find(filter).sort({ date: 1 })
+        const holidays = await Holiday.find().sort({ from_date: 1 })
         return res.status(200).json(holidays)
     }
     catch (err) {
@@ -80,11 +66,11 @@ async function updateHoliday(req, res) {
             return res.status(404).json({ message: 'Holiday not found.' })
         }
 
-        const { date, description, is_confirmed } = req.body
+        const { from_date, to_date, description, is_confirmed } = req.body
 
         const holiday = await Holiday.findByIdAndUpdate(
             req.params.id,
-            { date, description, is_confirmed },
+            { from_date, to_date, description, is_confirmed },
             { new: true, runValidators: true }
         )
 
@@ -93,7 +79,7 @@ async function updateHoliday(req, res) {
             recordId: holiday._id,
             userId: req.user._id,
             before,
-            after: { date, description, is_confirmed },
+            after: { from_date, to_date, description, is_confirmed },
             ipAddress: req.ip
         })
 
@@ -103,11 +89,6 @@ async function updateHoliday(req, res) {
         console.log(err)
         if (err.name === "ValidationError") {
             return res.status(400).json({ message: err.message })
-        }
-        if (err.code === 11000) {
-            return res.status(409).json({
-                message: 'A holiday already exists on this date.'
-            });
         }
         return res.status(500).json({ message: 'Internal Server Error' })
     }
